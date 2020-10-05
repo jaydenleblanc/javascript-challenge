5aa5b676a3497deb7cc105993fd84d2b478e10d9
Jayden LeBlanc <jaydenleblanc@gmail.com>
2020-10-04 22:39:53 -0400

    All data in table

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -16,14 +16,15 @@ function createTable(datetime, city, state, country, shape, comment) {
   var table = d3.select("#ufo-table");
   var tbody = table.select("tbody");
   var trow;
-  for (var i = 0; i < 12; i++) {
+  for (var i = 0; i < 111; i++) {
     trow = tbody.append("tr");
-    trow.append("td").text(datetime);
-    trow.append("td").text(city);
-    trow.append("td").text(state);
-    trow.append("td").text(country);
-    trow.append("td").text(shape);
-    trow.append("td").text(comment);
+    trow.append("td").text(data[i].datetime);
+    trow.append("td").text(data[i].city);
+    trow.append("td").text(data[i].state);
+    trow.append("td").text(data[i].country);
+    trow.append("td").text(data[i].shape);
+    trow.append("td").text(data[i].durationMinutes);
+    trow.append("td").text(data[i].comments);
   }
 }
 
